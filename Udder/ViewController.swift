c3ceb333c588eeddae0195c8f6540875f1167a59
Maria <maria.ignatova93@gmail.com>
2022-01-08 13:20:28 +0000

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Maria Ignatova on 1/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
